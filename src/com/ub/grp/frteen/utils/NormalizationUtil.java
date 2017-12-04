af0d8007ab8a7e13ea41c09335c18eb023ad03dd
varmax2511 <var_max@hotmail.com>
2017-12-04 16:28:35 -0500

    - Case handling for 0 scores

--- src/com/ub/grp/frteen/utils/NormalizationUtil.java
+++ src/com/ub/grp/frteen/utils/NormalizationUtil.java
@@ -3,32 +3,40 @@ package com.ub.grp.frteen.utils;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
-
+/**
+ * Utility class for score normalization.
+ * 
+ * @author varunjai
+ *
+ */
 public class NormalizationUtil {
-
+  /**
+   * This method is used to normalize the scores of all the members.
+   * 
+   * @param memberScores
+   *          The map of member name to member score
+   * @param totalScore
+   *          The total score of all the members
+   * @return
+   */
   public static Map<String, Double> getNormalizedScores(
       Map<String, Integer> memberScores, int totalScore) {
-    Map<String, Double> member2NormalizedScores = new HashMap<>();
+    final Map<String, Double> member2NormalizedScores = new HashMap<>();
 
-    for (Entry<String, Integer> memberScore : memberScores.entrySet()) {
+    for (final Entry<String, Integer> memberScore : memberScores.entrySet()) {
 
       // no score
       if (totalScore == 0) {
-        member2NormalizedScores.put(memberScore.getKey(), 0.0);
+        member2NormalizedScores.put(memberScore.getKey(),
+            1.0 / memberScores.size());
         continue;
       }
 
+      // compute score
       member2NormalizedScores.put(memberScore.getKey(),
-          Double.valueOf(memberScore.getValue()) /  totalScore);
-    }
+          Double.valueOf(memberScore.getValue()) / totalScore);
+    }// for
 
     return member2NormalizedScores;
-  }
-  
-  public static void main(String[] args){
-    Map<String, Integer> h = new HashMap<>();
-    h.put("v", 10);
-    h.put("c", 10);
-    getNormalizedScores(h, 20);
   }
 }
